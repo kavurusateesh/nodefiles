7827879365276139e133771a49ff872d954212bf
kavurusateesh <45224978+kavurusateesh@users.noreply.github.com>
2020-05-17 15:48:55 +0530

    Add files via upload

--- config/passport.js
+++ config/passport.js
@@ -8,51 +8,53 @@ var encryption = require('../helpers/Encryption');
 module.exports = function(passport) {
 //Serialize sessions
 passport.serializeUser(function (user, done) {
-    //console.log('serialize user???');
+ 
     done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-    User.findOne({
-        id: id
+    
+    User.findOne({where:{
+        'id': id}
     }, function (err, user) {
         if (user.id) user.userId = user.id;
         done(err, user);
     });
 });
 
-
-//Use local strategy
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-},
+ //Use local strategy
+ passport.use(new LocalStrategy( 
+    {
+        usernameField: 'email',
+        passwordField: 'password'
+    },
     function (email, password, done) {
-        console.log('fsecond',encryption.encryptData(email));
-        console.log('password',encryption.encryptData(password));
-        User.findOne({
-            email: encryption.encryptData(email)
-        }, function (err, user) {
-            if (err) {
-                console.log('err ps', JSON.stringify(err));
-                return done(err);
-            }
+      User.findOne( {where:{
+            "email": encryption.encryptData(email), "password":encryption.encryptData(password) }
+        }).then(function (user) {
+            
             if (!user) {
                 console.log('Unknown use');
                 return done(null, false, {
                     message: 'Unknown user'
                 });
             }
-            if (!encryption.encryptData(password)) {
-                console.log('Invalid passwordd')
-                return done(null, false, {
-                    message: 'Invalid passwordd'
-                });
-            }
-            console.log('chandra finala');
+            // if (!encryption.encryptData(password)) {
+            //     console.log('Invalid passwordd')
+            //     return done(null, false, {
+            //         message: 'Invalid passwordd'
+            //     });
+            // }
+          
             return done(null, user);
         });
+        // .catch(err => {
+        //     console.log('err ps', JSON.stringify(err));
+        //     return done(err);
+        // })
     }
 ));
+
+
 
 }
